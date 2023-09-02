13552618e58c457b8b66aae4474f654aecee5153
agnkos <101097155+agnkos@users.noreply.github.com>
2023-09-02 19:50:07 +0200

    exercise 3.8

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
-app.use(morgan('tiny'))
+app.use(morgan(':method :url :response-time :status :body'))
+
+morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
 
 let persons = [
     {
