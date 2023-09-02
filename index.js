c651f4f701cd9e4db9d65d64b8c49401df500dd9
agnkos <101097155+agnkos@users.noreply.github.com>
2023-09-02 18:24:31 +0200

    exercise 3.5

--- index.js
+++ index.js
@@ -50,6 +50,30 @@ app.get('/info', (request, response) => {
     response.send(`Phonebook has info for ${persons.length} people. <br /> ${date}`)
 })
 
+app.use(express.json())
+
+const generateId = () => {
+    const randomId = Math.floor(Math.random() * 1000)
+    return randomId
+}
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+
+    if (!body.name) {
+        return response.status(400).json({ error: 'name missing' })
+    }
+
+    const person = {
+        id: generateId(),
+        name: body.name,
+        number: body.number,
+    }
+
+    persons = persons.concat(person)
+    response.json(person)
+})
+
 const PORT = 3001
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
