78d4563d5c312e794ccde9d73e387fb5f1a9d540
agnkos <101097155+agnkos@users.noreply.github.com>
2023-09-02 18:09:12 +0200

    exercise 3.4

--- index.js
+++ index.js
@@ -38,6 +38,12 @@ app.get('/api/persons/:id', (request, response) => {
     }
 })
 
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.filter(p => p.id !== id)
+
+    response.status(204).end()
+})
 
 app.get('/info', (request, response) => {
     const date = new Date()
