3105bddb958540859a9c858f26d4b2bcc6a2b574
agnkos <101097155+agnkos@users.noreply.github.com>
2023-09-07 15:40:01 +0200

    exercise 3.13

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
-
 const app = express()
+require('dotenv').config()
+
+const Person = require('./models/person')
 
 app.use(express.json())
 app.use(morgan(':method :url :response-time :status :body'))
@@ -10,40 +12,45 @@ app.use(cors())
 app.use(express.static('dist'))
 
 let persons = [
-    {
-        "id": 1,
-        "name": "Arto Hellas",
-        "number": "040-123456"
-    },
-    {
-        "id": 2,
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523"
-    },
-    {
-        "id": 3,
-        "name": "Dan Abramov",
-        "number": "12-43-234345"
-    },
-    {
-        "id": 4,
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122"
-    }
+    // {
+    //     "id": 1,
+    //     "name": "Arto Hellas",
+    //     "number": "040-123456"
+    // },
+    // {
+    //     "id": 2,
+    //     "name": "Ada Lovelace",
+    //     "number": "39-44-5323523"
+    // },
+    // {
+    //     "id": 3,
+    //     "name": "Dan Abramov",
+    //     "number": "12-43-234345"
+    // },
+    // {
+    //     "id": 4,
+    //     "name": "Mary Poppendieck",
+    //     "number": "39-23-6423122"
+    // }
 ]
 
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    Person.find({}).then(persons => {
+        response.json(persons)
+    })
 })
 
 app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(p => p.id === id)
-    if (person) {
+    // const id = Number(request.params.id)
+    // const person = persons.find(p => p.id === id)
+    // if (person) {
+    //     response.json(person)
+    // } else {
+    //     response.status(404).end()
+    // }
+    Person.findById(request.params.id).then(person => {
         response.json(person)
-    } else {
-        response.status(404).end()
-    }
+    })
 })
 
 app.delete('/api/persons/:id', (request, response) => {
@@ -93,7 +100,7 @@ morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
 // app.listen(PORT)
 // console.log(`Server running on port ${PORT}`)
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
