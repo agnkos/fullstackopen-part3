9bc9ab09c7cf3b06a941d34e55cee7ee9bba81b9
agnkos <101097155+agnkos@users.noreply.github.com>
2023-09-08 11:20:32 +0200

    exercise 3.15

--- index.js
+++ index.js
@@ -11,29 +11,6 @@ app.use(morgan(':method :url :response-time :status :body'))
 app.use(cors())
 app.use(express.static('dist'))
 
-let persons = [
-    // {
-    //     "id": 1,
-    //     "name": "Arto Hellas",
-    //     "number": "040-123456"
-    // },
-    // {
-    //     "id": 2,
-    //     "name": "Ada Lovelace",
-    //     "number": "39-44-5323523"
-    // },
-    // {
-    //     "id": 3,
-    //     "name": "Dan Abramov",
-    //     "number": "12-43-234345"
-    // },
-    // {
-    //     "id": 4,
-    //     "name": "Mary Poppendieck",
-    //     "number": "39-23-6423122"
-    // }
-]
-
 app.get('/api/persons', (request, response) => {
     Person.find({}).then(persons => {
         response.json(persons)
@@ -53,11 +30,17 @@ app.get('/api/persons/:id', (request, response) => {
     })
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.filter(p => p.id !== id)
+app.delete('/api/persons/:id', (request, response, next) => {
+    // const id = Number(request.params.id)
+    // const person = persons.filter(p => p.id !== id)
 
-    response.status(204).end()
+    // response.status(204).end()
+
+    Person.findByIdAndRemove(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
 
 app.get('/info', (request, response) => {
@@ -98,11 +81,8 @@ app.post('/api/persons', (request, response) => {
         response.json(savedPerson)
     })
 })
-morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
 
-// const PORT = 3001
-// app.listen(PORT)
-// console.log(`Server running on port ${PORT}`)
+morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
